026d5bee7b8e2732bdc1f488190046ad60cf8c97
Preston Rohner <prestonrohner@me.com>
2010-01-05 20:44:13 -0800

    Adding in-place-editing

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100105055023) do
+ActiveRecord::Schema.define(:version => 20100106035440) do
 
   create_table "food_items", :force => true do |t|
     t.string   "name"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(:version => 20100105055023) do
 
   create_table "grocery_lists", :force => true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "in_place_editors", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "phone"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
