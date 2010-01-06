5e59f7102ec9fc80e4f7a2efa954264ff63507f9
Preston Rohner <prestonrohner@me.com>
2010-01-06 11:40:07 -0800

    Searching pagination results remotely

--- app/controllers/pagination_controller.rb
+++ app/controllers/pagination_controller.rb
@@ -2,19 +2,27 @@ class PaginationController < ApplicationController
   ## http://www.smashingmagazine.com/2007/11/16/pagination-gallery-examples-and-good-practices/
   
   def index
-    paginate_data
+    @pagination_items = PaginationItem.paginate :per_page => 5,
+                                                :page => params[:page],
+                                                :conditions => [ 'name like ?', "%#{params[:query]}%" ],
+                                                :order => :name
+    respond_to do |format|
+      format.html
+      format.js {
+        render :update do |page|
+          page.replace_html 'pagination_items_table', :partial => "pagination_items"
+        end
+      }
+    end
   end
   
-  def list
+  def list 
+    logger.error("LANDED IN LIST ACCIDENTALLY")
     paginate_data
   end
   
 private
 
   def paginate_data
-    @pagination_items = PaginationItem.paginate :per_page => 5,
-                                                :page => params[:page],
-                                                :conditions => [ 'name like ?', "%#{params[:query]}%" ],
-                                                :order => :name
   end
 end
