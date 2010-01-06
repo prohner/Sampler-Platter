89e237a11414d8cbf89c280cf52363cf594eb652
Preston Rohner <prestonrohner@me.com>
2010-01-06 11:26:40 -0800

    Dynamic update of table through Javascript search

--- app/controllers/pagination_controller.rb
+++ app/controllers/pagination_controller.rb
@@ -2,12 +2,19 @@ class PaginationController < ApplicationController
   ## http://www.smashingmagazine.com/2007/11/16/pagination-gallery-examples-and-good-practices/
   
   def index
-    @pagination_items = PaginationItem.paginate :per_page => 5,
-                                                :page => params[:page],
-                                                :conditions => [ 'name like ?', "%#{params[:search]}" ],
-                                                :order => :name
+    paginate_data
   end
   
   def list
+    paginate_data
+  end
+  
+private
+
+  def paginate_data
+    @pagination_items = PaginationItem.paginate :per_page => 5,
+                                                :page => params[:page],
+                                                :conditions => [ 'name like ?', "%#{params[:query]}%" ],
+                                                :order => :name
   end
 end
