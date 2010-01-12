e4666f1a793f29df877a6c4a48dd41f97521d933
Preston Rohner <prestonrohner@me.com>
2010-01-11 20:18:58 -0800

    Move around notes

--- app/models/people.rb
+++ app/models/people.rb
@@ -1,2 +1,6 @@
 class People < ActiveRecord::Base
+  ## Don't quite know how this is going to workout
+  ## http://github.com/rails-sqlserver/2000-2005-adapter#readme
+  ## http://odbc-rails.rubyforge.org/
+  
 end
