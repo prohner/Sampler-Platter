4beb69e5f0f8f7da77ed1b404a7918a12cc36902
Preston Rohner <prestonrohner@me.com>
2010-01-11 20:16:56 -0800

    Second marker and popup info window

--- app/controllers/google_map_controller.rb
+++ app/controllers/google_map_controller.rb
@@ -2,12 +2,18 @@ class GoogleMapController < ApplicationController
   ## http://www.developer.com/open/article.php/10930_3757576_1/Adding-Google-Maps-To-Your-Rails-Applications.htm
   
   def index
+    origin = [34.145227, -118.756588]
     @map = GMap.new("map")
     @map.control_init(:large_map => true, :map_type => true)
-    @map.center_zoom_init([41, -80], 10)
+    @map.center_zoom_init(origin, 10)
+
+    home_address = "123 Lindero Canyon<br />Oak Park, CA 91377<br /><a href=""http://www.google.com"">Google</a>"
     
-    ianazones = GMarker.new([41, -80])
-    @map.overlay_init(ianazones)
+    oak_park = GMarker.new(origin, :title => "The Address", :info_window => "#{home_address}")
+    @map.overlay_init(oak_park)
+
+    woodland_hills = GMarker.new([34.165227, -118.60])
+    @map.overlay_init(woodland_hills)
   end
 
 end
