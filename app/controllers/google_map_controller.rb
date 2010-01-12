1277c9ff8d241f8037c52c953aa54d55fc5fc92d
Preston Rohner <prestonrohner@me.com>
2010-01-11 16:50:37 -0800

    Adding gmaps

--- app/controllers/google_map_controller.rb
+++ app/controllers/google_map_controller.rb
@@ -1,4 +1,6 @@
 class GoogleMapController < ApplicationController
+  ## http://www.developer.com/open/article.php/10930_3757576_1/Adding-Google-Maps-To-Your-Rails-Applications.htm
+  
   def index
     @map = GMap.new("map")
     @map.control_init(:large_map => true, :map_type => false)
