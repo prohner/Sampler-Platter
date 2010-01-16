19759f13b95c974bf407ce3edff3a80aae339fec
Preston Rohner <prestonrohner@me.com>
2010-01-15 20:41:38 -0800

    Added a calendar helper plugin

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -3,5 +3,10 @@ class SamplesController < ApplicationController
   
   def index
   end
-
+  
+  def calendar
+    y = params.include?(:year) ? params[:year].to_i : Date.today.year
+    m = params.include?(:month) ? params[:month].to_i : Date.today.month
+    @display_date = Date.new(y, m, 1)
+  end
 end
--- app/helpers/samples_helper.rb
+++ app/helpers/samples_helper.rb
@@ -1,2 +1,18 @@
 module SamplesHelper
+
+  def calendar_options                                        
+    prevd = @display_date - 1
+    nextd = @display_date + 31
+    {       
+      :year => @display_date.year,
+      :month => @display_date.month,
+      :previous_month_text =>
+        link_to("&lt;&lt; Prev",
+          :year => prevd.year, :month => prevd.month),                                               
+      :next_month_text =>                                     
+        link_to("Next &gt;&gt;",
+          :year => nextd.year, :month => nextd.month)
+    }                                                         
+  end
+  
 end
